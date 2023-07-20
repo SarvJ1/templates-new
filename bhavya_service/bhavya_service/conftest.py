06a2902b91c346c285a50a7b923703083a747890
Test <test@harness.io>
2023-07-20 10:23:48 +0000

    Initial Django Service Setup

---
+++ bhavya_service/bhavya_service/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from bhavya_service.users.models import User
+from bhavya_service.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
---
+++ bhavya_service/config/settings/test.py
@@ -0,0 +1,32 @@
+"""
+With these settings, tests run faster.
+"""
+
+from .base import *  # noqa
+from .base import env
+
+# GENERAL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="fBfHP3cLhXcIgLt4MtYAwpmn3rxKl7RIEDTeu6cUZ4RVEDNF08SfAve9BZWb7L5E",
+)
+# https://docs.djangoproject.com/en/dev/ref/settings/#test-runner
+TEST_RUNNER = "django.test.runner.DiscoverRunner"
+
+# PASSWORDS
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#password-hashers
+PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
+
+# EMAIL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
+
+# DEBUGGING FOR TEMPLATES
+# ------------------------------------------------------------------------------
+TEMPLATES[0]["OPTIONS"]["debug"] = True  # type: ignore # noqa: F405
+# Your stuff...
+# ------------------------------------------------------------------------------
